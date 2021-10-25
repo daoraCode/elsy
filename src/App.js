833805133c08f19fce91080c0dcb43f780ef90cc
daoraCode <severin.mboukou@gmail.com>
2021-10-25 16:33:43 +0200

    updated files as Box components and props use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import Box from './components/Box';
+import global from './styles/global.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <Box icon='local_drink' color='#3A85FF' value='1.5' unit='L' />
+          <Box icon='directions_walk' color='black' value='3000' unit='steps' />
+          <Box icon='favorite' color='red' value='120' unit='bpm' />
+          <Box icon='wb_sunny' color='yellow' value='-10' unit='Celsius' />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Box extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className='box col-sm-3 col-6'>
+        <span
+          class='material-icons'
+          style={{ fontSize: 100, color: this.props.color }}
+        >
+          {this.props.icon}
+        </span>
+        <p>
+          {this.props.value} {this.props.unit}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Box;
